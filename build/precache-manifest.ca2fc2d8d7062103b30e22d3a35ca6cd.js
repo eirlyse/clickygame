859ea369c4e636678fe2ccc47eb71ec74a89ca9c
Eirlyse <eirlyse.s@gmail.com>
2020-01-14 20:42:41 -0700

    deploy

---
+++ build/precache-manifest.ca2fc2d8d7062103b30e22d3a35ca6cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9513558ff83d1f95a668e664c11fb43",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "c1d80be229b1ed744398",
+    "url": "/clickygame/static/css/main.afa5db18.chunk.css"
+  },
+  {
+    "revision": "b20c88718038e8c91cc9",
+    "url": "/clickygame/static/js/2.3db5c84d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickygame/static/js/2.3db5c84d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1d80be229b1ed744398",
+    "url": "/clickygame/static/js/main.bb33cca9.chunk.js"
+  },
+  {
+    "revision": "4d95e025c74c73140b85",
+    "url": "/clickygame/static/js/runtime-main.2999711a.js"
+  }
+]);
